b45ba42836d3d2827b5db42774c3647b6ad4d04d
JoseCourby <josecourby@gmail.com>
2022-12-20 21:01:37 -0300

    Se suben Express y Node

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require ('express');
+
+const app = express ();
+const path = require ('path');
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(3026,() => {
+
+console.log('se prendio');
+
+});
+
+app.get ('/',(req,res)=>{
+
+res.sendFile(path.resolve(__dirname,'views/home.html'));
+
+});
+
+app.get ('/login',(req,res)=>{
+
+    res.sendFile(path.resolve(__dirname,'views/login.html'));
+    
+    });
