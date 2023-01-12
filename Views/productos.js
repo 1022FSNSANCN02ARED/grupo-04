af69bea4f92dc5440209a7f795a7d274ed150ed2
JnOlivera <javiernicolasolivera@gmail.com>
2023-01-12 19:44:26 -0300

    estilos y responsive de productos

---
+++ Views/productos.js
@@ -0,0 +1,16 @@
+const inputQuantity = document.querySelector ('.input-quantity')
+const btnIncrement = document.querySelector ('#increment')
+const btnDecrement = document.querySelector ('#decrement')
+
+let valueByDeFault = parseInt (inputQuantity.value)
+
+btnIncrement.addEventListener('click', () =>{
+valueByDeFault += 1
+inputQuantity.value = valueByDeFault
+})
+
+btnDecrement.addEventListener('click', () =>{
+valueByDeFault -= 1
+inputQuantity.value = valueByDeFault
+})
+
