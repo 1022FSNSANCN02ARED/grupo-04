9f852e11f913551b3b2d637c3471fe7a71b43c2a
JnOlivera <javiernicolasolivera@gmail.com>
2023-01-28 13:17:26 -0300

    se agrega sprint4.ejs donde te dirige a detalle de producto, crear producto, editar producto, lista de producto. Se usa metodo Get y Post para crear un producto

--- routers/mainRouter.js
+++ routers/mainRouter.js
@@ -15,6 +15,18 @@ router.get("/tienda",mainController.tienda);
 /* Rutas de productsController */
 router.get("/list",productsController.allProducts);
 router.get("/create", productsController.create);
+router.get("/mas", productsController.create);
+router.get("/new", productsController.new);
+
+/*recibir informacion del producto creado*/
+
+
+router.post ('/mainRouter', async (req,res)=>{
+
+    const body = req.body;
+    console.log(body);
+})
+
 
 
 module.exports = router;
