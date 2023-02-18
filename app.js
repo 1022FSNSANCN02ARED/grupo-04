c856ef5ca473af262f512cecf5ef15b8430fc894
Fromero564 <fervicromero@gmail.com>
2023-02-17 21:02:23 -0300

    Se agregan cambios

--- app.js
+++ app.js
@@ -45,72 +45,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use('/', router);
 
 
-/*Vistas sin renderizar*/
 
-
-app.get ('/productoHombre',(req,res)=>{
-
-    res.sendFile(path.resolve(__dirname,'views/products/productoHombre.html'));
-    
-    });
-app.get ('/productoMujer',(req,res)=>{
-
-    res.sendFile(path.resolve(__dirname,'views/products/productoMujer.html'));
-    
-    });
-app.get ('/unisex',(req,res)=>{
-
-    res.sendFile(path.resolve(__dirname,'views/products/unisex.html'));
-    
-    });
-app.get ('/cuadro',(req,res)=>{
-
-    res.sendFile(path.resolve(__dirname,'views/products/cuadro.html'));
-    
-    });
-app.get ('/cuadro1',(req,res)=>{
-
-    res.sendFile(path.resolve(__dirname,'views/products/cuadro1.html'));
-    
-    });
-
-/*Probando session*/
-app.get('/pruebaSession',function(req,res){
-    if(req.session.numeroVisitas == undefined){
-        req.session.numeroVisitas = 0;
-    }
-    req.session.numeroVisitas ++;
- res.send('Session tiene el numero:'+req.session.numeroVisitas);   
-});
-
-/*Vistas anteriores*/
-/*
-app.get ('/list',(req,res)=>{
-
-    res.render('products/list',{ products: products.findAll() });
-    });
-
-app.get ('/registro',(req,res)=>{
-
-     res.sendFile(path.resolve(__dirname,'views/registro.html'));
-        
-    });
-
- app.get ('/carrito',(req,res)=>{
-
-        res.sendFile(path.resolve(__dirname,'views/carrito.html'));
-        
-        });
-app.get ('/tienda',(req,res)=>{
-
-        res.sendFile(path.resolve(__dirname,'views/tienda.html'));
-        
-        });
-
-        */
-
-
-
-        app.use((req,res,next)=>{
-            res.status(404).render('notFound')
-    })
+ app.use((req,res,next)=>{
+ res.status(404).render('notFound')
+ })
