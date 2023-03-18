270be2898554d4a744ca508cfd352a2bed7d91af
JnOlivera <javiernicolasolivera@gmail.com>
2023-03-18 12:43:41 -0300

    rutas y controladores de categorias. Estilo del menu desplegable de categorias

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,7 +2,8 @@ const path = require("path");
 const { saveProduct, findById } = require("../data/products");
 const products = require("../data/products");
 const fs = require("fs");
-const carritoFilePath =path.resolve("data/carrito.json");
+const carritoFilePath = path.resolve("data/carrito.json");
+const productosfilepath = path.resolve('data/productsDatos.json')
 
 module.exports = {
   Home: (req,res)=> {
@@ -19,8 +20,29 @@ module.exports = {
     return res.render('products/tienda', { products: products.findAll(),estado});
   },
   nosotros:(req,res)=>{
-    return res.render('users/nosotros');
+    return res.render('users/nosotros'); 
   },
+  decoracion: (req, res) => {
+    let decoracion = fs.readFileSync(productosfilepath, 'utf8');
+    let pTotales = JSON.parse (decoracion);
+    let pDecoracion = pTotales.filter(p => p.category == 'decoracion')
+    res.render('products/decoracion', {pDecoracion})  
+       },
+  hogar: (req, res) => {
+    let hogar = fs.readFileSync(productosfilepath, 'utf8');
+    let pTotales = JSON.parse (hogar);
+    let pHogar = pTotales.filter(p => p.category == 'hogar')
+    res.render('products/hogar', {pHogar})  
+       },
+  indumentaria: (req, res) => {
+    let indumentaria = fs.readFileSync(productosfilepath, 'utf8');
+    let pTotales = JSON.parse (indumentaria);
+    let pIndumentaria = pTotales.filter(p => p.category == 'indumentaria')
+    res.render('products/indumentaria', {pIndumentaria})  
+       },
+   
+
+  
   carrito:(req,res)=>{
     let id = req.params.id;
     let productos = products.findById(id);
@@ -57,6 +79,7 @@ module.exports = {
 
   donaciones:(req,res)=>{
     return res.render('users/donaciones');
+    
   },
 };
 
