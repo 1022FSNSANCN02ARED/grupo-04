3c3b1a7f5387fe76a92602fab40d814d57ed1e9f
Fromero564 <fervicromero@gmail.com>
2023-05-05 16:19:17 -0300

    Se traba en frontend con react

--- Frontend/src/components/UserDetails.jsx
+++ Frontend/src/components/UserDetails.jsx
@@ -1,27 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-function UserDetails() {
+
+function UserDetails (){
   const { id } = useParams();
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState([]);
+ 
+
+ 
 
   useEffect(() => {
-    fetch(`http://localhost:3026/api/user/${id}`)
+    fetch("http://localhost:3026/api/user")
       .then((response) => response.json())
       .then((data) => setUser(data.data));
   }, [id]);
+  
+  const userFiltrado = user.find(u => u.id == id);
+  
 
-  if (!user) {
+  if (!userFiltrado) {
     return <p>Cargando...</p>;
   }
 
   return (
     <>
-      <h1>{user.nombre}</h1>
-      <p>{user.email}</p>
-      <p>{user.pais}</p>
+     <p>Id:{userFiltrado.id}</p>
+     <p>Nombre:{userFiltrado.nombre}</p>
+     <p>Email:{userFiltrado.email}</p>
+     <p>País:{userFiltrado.pais}</p>
+     <p>Telefono:{userFiltrado.telefono}</p>
+     
     </>
   );
 }
+
 
 export default UserDetails;
